6f89994fbc3e1bd6b21fa50cba37ec335631412a
S1nOfSloth <151624696+S1nOfSloth@users.noreply.github.com>
2024-04-09 18:18:27 +1000

    Update Lab_3.c

--- Lab_3.c
+++ Lab_3.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
 
+int main(){
+    char mass[80];
+    
+    printf("Введите строку до 80 символов: ");
+    scanf("%s", mass);
+  
+    for (int i = 0; i < 80; i++){
+        if (mass[i] == 'a'){
+            mass[i] = 'A';
+        }
+        else if (mass[i] == 'b'){
+            mass[i] = 'B';
+        }
+        else if (mass[i] == '+'){
+            mass[i] = '-';
+        }
+    }
+    printf("Итоговая строка: ");
+    printf("%s", mass);
+  
+  return 0;
+}
